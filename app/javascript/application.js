4b547611f7896d8294f3ade8010aa640d1baccfc
Fabrice Tuilliere <fabrice.tui@gmail.com>
2022-11-23 15:22:57 +0100

    add recorder

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,15 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
+import { audioRecord } from "./pack/audio"
+
+// require("@rails/ujs").start()
+// require("turbolinks").start()
+// require("@rails/activestorage").start()
+// require("channels")
+
+// document.addEventListener("turbolinks:load", function() {
+//   audioRecord()
+// });
+
+audioRecord();
